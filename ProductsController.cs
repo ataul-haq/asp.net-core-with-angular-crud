655a1cf8af1612c9d065b4ade2dce5fc500942fb
Ataul Haq <61939870+ataul-haq@users.noreply.github.com>
2020-03-10 00:58:34 +0600

    Add files via upload

---
+++ ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using new_1;
+using new_1.Models;
+
+namespace new_1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace new_1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private my_DBEntities db = new my_DBEntities();
+        public IEnumerable<Product> Get()
+        
+        {
+            return db.Product.ToList();
+        }
+        public Product Get(int id)
+        {
+            return db.Product.Find(id);
+        }
+        public void Post(Product value)
+        {
+            Product obj = new Product();
+            obj.Name = value.Name;
+            obj.Category = value.Category;
+            obj.CreateDate = DateTime.Now;
+            obj.Price = value.Price;
+            db.Product.Add(obj);
+            db.SaveChanges();
+        }
+        public void Put(Product value)
+        {
+            db.Entry(value).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            Product product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+        }
+    }
+}
