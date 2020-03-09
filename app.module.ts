147492485bac73fdd64e39d719bce535fda80e04
Ataul Haq <61939870+ataul-haq@users.noreply.github.com>
2020-03-10 01:03:35 +0600

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      UserComponent,
+      HeaderComponent,
+      FooterComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
